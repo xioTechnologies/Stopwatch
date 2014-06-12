c7c15347835be69e5898d87b4e790a1ceb0f04b5
xioTechnologies <social@x-io.co.uk>
2014-06-12 16:06:11 +0100

    Add project files

---
+++ Stopwatch/Stopwatch/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Stopwatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTime = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 37F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTime.Location = new System.Drawing.Point(12, 9);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(319, 58);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "00:00:00:000";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStart.Location = new System.Drawing.Point(90, 72);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStop.Location = new System.Drawing.Point(171, 72);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonReset.Location = new System.Drawing.Point(252, 72);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 3;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 107);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelTime);
+            this.MinimumSize = new System.Drawing.Size(355, 145);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonReset;
+    }
+}
+
---
+++ Stopwatch/Stopwatch/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace Stopwatch
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Stopwatch object
+        /// </summary>
+        private System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
+
+        /// <summary>
+        /// Timer object
+        /// </summary>
+        private System.Windows.Forms.Timer formUpdateTimer = new Timer();
+
+        // Constructor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Form load event
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = Assembly.GetExecutingAssembly().GetName().Name;
+            Form1_Resize(this, null);
+            stopwatch = new System.Diagnostics.Stopwatch();
+            formUpdateTimer.Interval = 20;
+            formUpdateTimer.Tick += new EventHandler(formUpdateTimer_Tick);
+            formUpdateTimer.Start();
+        }
+
+        /// <summary>
+        /// Resize form event
+        /// </summary>
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            labelTime.Font = new Font(labelTime.Font.FontFamily, (float)this.Width / 9);
+            labelTime.Location = new Point(0, (this.Height / 2) - (int)(labelTime.Font.Size / 2.0f) - 50);
+        }
+
+        /// <summary>
+        /// Forum update timer event
+        /// </summary>
+        void formUpdateTimer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan t = new TimeSpan((long)((double)stopwatch.ElapsedTicks * (1000.0 * 1000.0 * 10.0 / (double)(System.Diagnostics.Stopwatch.Frequency))));
+            labelTime.Text = string.Format("{0:D2}:{1:D2}:{2:D2}:{3:D3}", t.Hours, t.Minutes, t.Seconds, t.Milliseconds);
+        }
+
+        /// <summary>
+        /// Start button event
+        /// </summary>
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            stopwatch.Start();
+        }
+
+        /// <summary>
+        /// Stop button event
+        /// </summary>
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            stopwatch.Stop();
+        }
+
+        /// <summary>
+        /// Reset button event
+        /// </summary>
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            if (stopwatch.IsRunning)
+            {
+                stopwatch = new System.Diagnostics.Stopwatch();
+                stopwatch.Start();
+            }
+            else
+            {
+                stopwatch = new System.Diagnostics.Stopwatch();
+            }
+        }
+    }
+}
